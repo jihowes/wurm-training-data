99327e17826b7f18f76301f0bbe00a13e9df7522
jihowes <jihowes.jh@gmail.com>
2025-11-09 15:43:28 +1100

    Remove hero scroll indicator

--- frontend/components/Hero.tsx
+++ frontend/components/Hero.tsx
@@ -110,12 +110,6 @@ export default function Hero() {
         </div>
       </div>
 
-      {/* Scroll indicator */}
-      <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 animate-bounce">
-        <svg className="w-6 h-6 text-purple-400" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
-          <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
-        </svg>
-      </div>
     </section>
   );
 }
